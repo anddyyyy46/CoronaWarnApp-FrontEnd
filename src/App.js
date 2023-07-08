124ccd85f4660a92a687c1ee5083dcd0fb979216
Andreas Rautenberg <andyrau99@gmail.com>
2023-07-08 23:36:40 +0200

    added styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import { Line } from "react-chartjs-2"
 import { useEffect, useState } from "react"
 import {LineChart} from "./Components/Graph"
-import { Calender } from "./Components/Calender"
-import { Range } from "./Components/Range"
 import "./App.css"
 
 function App() {
@@ -12,6 +9,9 @@ function App() {
   const [apiData, setapiData] = useState([])
   const [apiDataAvrg, setapiDataAvrg] = useState([])
   const [date, setDate] = useState("")
+  const [displayedRangeAvrg, setDisplayedRangeAvrg] = useState("2021")
+  const [displayedRange, setDisplayedRange] = useState("17.03.2021-25.07.2021")
+  
 
   useEffect(()=>{
       getDataAvrg()
@@ -19,52 +19,48 @@ function App() {
   },[])
 
   const getDataAvrg = async()=>{
-      await fetch(`https://coronawarnappdaten.onrender.com/getDataAverages?date=2021`).then((res) => res.json()).then((daten) =>setapiDataAvrg(daten))
+      await fetch(`https://coronawarnappdaten.onrender.com/getDataAverages?date=${displayedRangeAvrg}`).then((res) => res.json()).then((daten) =>setapiDataAvrg(daten))
   }
   const getData = async()=>{
-    await fetch(`https://coronawarnappdaten.onrender.com/getData?date=17.03.2021-25.07.2021`).then((res) => res.json()).then((daten) =>setapiData(daten))
+    await fetch(`https://coronawarnappdaten.onrender.com/getData?date=${displayedRange}`).then((res) => res.json()).then((daten) =>setapiData(daten))
   }
 
   let chartDataAvrg = {
     labels: apiDataAvrg.map((dayData) => dayData.month), //hier gucken wann was 
     datasets: [{
-      label:"Infections per Day",
-      data: apiDataAvrg.map((dayData) => dayData.average),
-      onClick: (e)=>{
-        console.log("dfksgj,n")
-      }
-      
+      label:"Average infections per day in a month",
+      data: apiDataAvrg.map((dayData) => dayData.average)
     }]
   }
   let chartDataAll = {
     labels: apiData.map((dayData) => dayData.date),
     datasets: [{
-      label:"Infections per Day",
+      label:"Infections per day",
       data: apiData.map((dayData) => dayData.infections_published_daily),
       borderColor: 'blue',
       backgroundColor: 'blue',
-      pointRadius: 0,
+      pointRadius: 0, //points removed because they are too much in a big Range
     },{
-      label: "app_downloads_daily",
+      label: "app downloads daily",
       data: apiData.map((dayData) => dayData.app_downloads_daily),
       borderColor: 'red',
       backgroundColor: 'red',
-      pointRadius: 0,
-      onClick: (e)=>{
-        console.log("dfksgj,n")
-      }
+      pointRadius: 0 //points removed because they are too much in a big Range
     }]
   }
   const fetchDataAvrg = async(datum)=>{
     await fetch(urlArvg+datum).then((res) => res.json()).then((daten) =>setapiDataAvrg(daten))
+    setDisplayedRangeAvrg(datum)
   }
   const fetchData = async(datum)=>{
     await fetch(url+datum).then((res) => res.json()).then((daten) =>setapiData(daten))
+    setDisplayedRange(datum)
   }
   
 
   return (
     <div className="App">
+    <h1>Displayed: {displayedRangeAvrg}</h1>
       <div className="eins">
         <div className="einseins">
           <LineChart chartData={chartDataAvrg}/>
@@ -75,22 +71,32 @@ function App() {
           </div>
           */}
           <div className="einsdrei">
-            <button onClick={()=>fetchDataAvrg("2020")}>2020</button>
+            <button onClick={() => fetchDataAvrg("2020")}>2020</button>
           </div>
           <div className="einsvier">
-            <button onClick={()=>fetchDataAvrg("2021")}>2021</button>
+            <button onClick={() => fetchDataAvrg("2021")}>2021</button>
           </div>
           <div className="einsfunf">
-            <button onClick={()=>fetchDataAvrg("2022")}>2022</button>
+            <button onClick={() => fetchDataAvrg("2022")}>2022</button>
           </div>
         </div>
       </div>
-      <p>Click to remove</p>
+      <hr/>
+      <h1>Displayed: {displayedRange}</h1>
       <div className="zwei">
         <LineChart chartData={chartDataAll}/>
         <div className="zweieins">
-          <input type="text" placeholder="pattern: mm.YYYY or YYYY or dd.mm.yyyy-dd.mm.yyyy" onKeyDown={(e)=> e.key==="Enter"&&fetchData(date)} onChange={(e)=> setDate(e.target.value)} />
-          <button onClick={()=> fetchData(date)}>Submit</button>
+          <input type="text" placeholder="e.g. 17.03.2021-25.07.2021" onKeyDown={(e)=> e.key==="Enter"&&fetchData(date)} onChange={(e)=> setDate(e.target.value)} />
+          <div className="infos">
+            <p>Data range: 01.01.2020-05.07.2022</p>
+            <p>Patterns:</p>
+            <ul>
+              <li>YYYY for example: 2021</li>
+              <li>MM.YYYY for example: 02.2021</li>
+              <li>DD.MM.YYYY-DD.MM.YYYY for example: 01.01.2021-01.02.2022</li>
+            </ul>
+          </div>
+          <button onClick={() => fetchData(date)}>Submit</button>
         </div>
       </div>
     </div>
--- src/Components/Range.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react"
-import "./Range.css"
-export const Range = ()=>{
-    return (<div className="test">
-        <input type="text" placeholder="example 01.01.2021-02.02.2021"   />
-        
-
-    </div>)
-}
